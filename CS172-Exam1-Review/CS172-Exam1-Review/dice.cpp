30e5784ce93d2f34de577429979549b404264e21
Heidi Wiebers <hwiebers19@hotmail.com>
2016-10-05 16:26:37 -0700

    fixed

--- CS172-Exam1-Review/CS172-Exam1-Review/dice.cpp
+++ CS172-Exam1-Review/CS172-Exam1-Review/dice.cpp
@@ -43,8 +43,47 @@ int Dice::getSides()
 }
 
 
+//
+//  Dice.cpp
+//  CS172
+//
+//  Created by Stephen Johnson on 10/4/16.
+//  Copyright © 2016 Glacier Peak Studios LLC. All rights reserved.
+//
 
+#include "Dice.hpp"
+#include <iostream>
+#include <cstdlib>
 
+Dice::Dice(int sides)
+{
+    //Check that the number of sides is valid
+    if (sides <= 0)
+    {
+        sides = 6;
+    }
+    
+    numSides = sides;
+    numRolls = 0;
+}
+
+int Dice::Roll()
+{
+    //Increment the number of times this dice was rolled.
+    numRolls++;
+    
+    //The random number generate is seeded in main
+    return (rand() % numSides) + 1;
+}
+int Dice::GetRolls()
+{
+    return numRolls;
+}
+
+int Dice::GetSides()
+{
+    return numSides;
+}
 
 
 
--- CS172-Exam1-Review/CS172-Exam1-Review/dice.hpp
+++ CS172-Exam1-Review/CS172-Exam1-Review/dice.hpp
@@ -28,3 +28,29 @@ public:
 };
 
 #endif /* dice_hpp */
+//this represents a dice of n sides
+class Dice
+{
+private:
+    int numSides;
+    int numRolls;
+    
+public:
+    
+    //creates a dice with the given number of sides.
+    //if the sides is less than or equal to 0 then
+    //this creates a 6 sided dice.
+    Dice(int sides);
+    
+    //Rolls the dice and returns a random number
+    //between 1 and the number of sides on the dice.
+    int Roll();
+    
+    //Returns the number of times this dice was rolled.
+    int GetRolls();
+    
+    //Return the number of sides on this dice.
+    int GetSides();
+};
+
+#endif /* Dice_hpp */
